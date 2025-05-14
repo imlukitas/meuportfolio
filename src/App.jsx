db2a94b9517bdf5d7a7d2478314f1e719e42d72f
Lucas Costa de Pinho <146649937+imlukitas@users.noreply.github.com>
2025-05-14 11:32:41 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,29 @@ import style from './App.module.css'
 function App() {
   return (
     <>
-      <h1 className={style.title}>Olá mundo</h1>
+      <nav className={'style.menu'}>
+        <p><a href="#s1">Apresentação</a></p>
+        <p><a href="#s2">Tecnologias</a></p>
+        <p><a href="#s3">Projetos</a></p>
+        <p><a href="#s4">Contato</a></p>
+      </nav>
+      <main>
+        <section className='style.s1'>
+          <h2>Apresentação</h2>
+        </section>
+        <section className='style.s2'>
+          Tecnologias
+        </section>
+        <section className='style.s3'>
+          Projetos
+        </section>
+        <section className='style.s4'>
+          Contato
+        </section>
+      </main>
+      <footer>
+        <h3>Redes Sociais</h3>
+      </footer>
     </>
   )
 }
